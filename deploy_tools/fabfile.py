6d3653ea23c6255d660d36b5b020fb9793979202
Jim Maciejewski <magicsoftware@ornear.com>
2023-01-27 08:14:02 +1000

    Updating fab file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,5 +1,4 @@
 from fabric import task
-import os
 import random
 import getpass
 
@@ -13,7 +12,7 @@ REPO_URL = f'git@bitbucket.org:itsmagic/{REPO_NAME}.git'
 
 @task
 def deploy(c):
-    site_folder = f'/home/{c.user}/sites/{REPO_NAME}'
+    site_folder = f'/home/{c.user}/sites/{c.host}'
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(c, site_folder)
     _get_latest_source(c, source_folder)
@@ -22,7 +21,7 @@ def deploy(c):
     _update_virtualenv(c, source_folder)
     _update_static_files(c, source_folder)
     _update_database(c, source_folder)
-    _add_service(c, source_folder)
+    _add_service(c, source_folder)  # Add True to force service files to be updated
     _add_nginx_config(c, source_folder)
     _restart_service(c)
 
@@ -46,9 +45,6 @@ def _get_latest_source(c, source_folder):
         c.run(f'cd {source_folder} && git fetch')
     c.run(f'cd {source_folder} && git pull')
     c.run(f'cd {source_folder} && git reset --hard')
-    
-    # current_commit = c.run(f'cd {source_folder} && git log -n 1 --format=%H')
-    # c.run(f'cd {source_folder} && git reset --hard {current_commit.stdout.strip()}')
 
 def _create_or_update_dotenv(c, source_folder):
     if c.run(f'test -f {source_folder}/../.env', warn=True).failed:
@@ -66,15 +62,18 @@ def _create_or_update_dotenv(c, source_folder):
 
 def _update_service_files(c, source_folder):
     print("Updating nginx file")
-    nginx_service_path = source_folder + '/deploy_tools/firmware_finder.nginx.conf'
+    c.run(f"cd {source_folder}/deploy_tools && mv default.nginx.conf {c.host}.nginx.conf && mv default.service {c.host}.service")
+    nginx_service_path = source_folder + f'/deploy_tools/{c.host}.nginx.conf'
     c.run(f"sed -i 's/\*\*SERVER NAME\*\*/{c.host}/g' {nginx_service_path}")
-    c.run(f"sed -i 's/\*\*SITE NAME\*\*/{REPO_NAME}/g' {nginx_service_path}")
+    c.run(f"sed -i 's/\*\*SITE NAME\*\*/{c.host}/g' {nginx_service_path}")
+    c.run(f"sed -i 's/\*\*USER NAME\*\*/{c.user}/g' {nginx_service_path}")
 
     print("Updating service file")
-    service_path = source_folder + '/deploy_tools/firmware_finder.service'
+    service_path = source_folder + f'/deploy_tools/{c.host}.service'
     c.run(f"sed -i 's/\*\*PROJECT NAME\*\*/{PROJECT_NAME}/g' {service_path}")
     c.run(f"sed -i 's/\*\*SERVER NAME\*\*/{c.host}/g' {service_path}")
-    c.run(f"sed -i 's/\*\*SITE NAME\*\*/{REPO_NAME}/g' {service_path}")
+    c.run(f"sed -i 's/\*\*SITE NAME\*\*/{c.host}/g' {service_path}")
+    c.run(f"sed -i 's/\*\*USER NAME\*\*/{c.user}/g' {service_path}")
 
 def _update_virtualenv(c, source_folder):
     virtualenv_folder = source_folder + '/../virtualenv' # maybe this needs work
@@ -93,22 +92,22 @@ def _update_database(c, source_folder):
 
 
 def _add_service(c, source_folder, force=False):
-    if c.run(f'test -f /etc/systemd/system/{REPO_NAME}.service', warn=True).failed or force:
+    if c.run(f'test -f /etc/systemd/system/{c.host}.service', warn=True).failed or force:
         # The service file doesn't exist, lets add it
         print("Adding service file")
         _get_sudo(c)
-        c.sudo(f'cp {source_folder}/deploy_tools/{REPO_NAME}.service /etc/systemd/system/{REPO_NAME}.service', hide='stderr')
-        c.sudo(f'systemctl enable {REPO_NAME}.service', hide='stderr')
+        c.sudo(f'cp {source_folder}/deploy_tools/{c.host}.service /etc/systemd/system/{c.host}.service', hide='stderr')
+        c.sudo(f'systemctl enable {c.host}.service', hide='stderr')
         c.sudo(f'systemctl daemon-reload', hide='stderr')
-        c.sudo(f'systemctl start {REPO_NAME}.service', hide='stderr')
+        c.sudo(f'systemctl start {c.host}.service', hide='stderr')
         
 def _add_nginx_config(c, source_folder, force=False):
-    if c.run(f'test -f /etc/nginx/sites-available/{REPO_NAME}.nginx.conf', warn=True).failed or force:
+    if c.run(f'test -f /etc/nginx/sites-available/{c.host}.nginx.conf', warn=True).failed or force:
         # The config file doesn't exist, lets add it
         print("Adding nginx config file")
         _get_sudo(c)
-        c.sudo(f'cp {source_folder}/deploy_tools/{REPO_NAME}.nginx.conf /etc/nginx/sites-available/{REPO_NAME}.nginx.conf', hide='stderr')
-        c.sudo(f'ln -sf /etc/nginx/sites-available/{REPO_NAME}.nginx.conf /etc/nginx/sites-enabled/', hide='stderr')
+        c.sudo(f'cp {source_folder}/deploy_tools/{c.host}.nginx.conf /etc/nginx/sites-available/{c.host}.nginx.conf', hide='stderr')
+        c.sudo(f'ln -sf /etc/nginx/sites-available/{c.host}.nginx.conf /etc/nginx/sites-enabled/', hide='stderr')
         check_config = c.sudo(f'nginx -t', hide='stderr')
         if check_config.stdout != "":
             print("NGINX config failed test, not restarting nginx")
@@ -117,7 +116,8 @@ def _add_nginx_config(c, source_folder, force=False):
             c.sudo(f'systemctl restart nginx', hide='stderr')
 
 def _restart_service(c):
-    c.run(f"sudo systemctl restart {REPO_NAME}.service")
+    _get_sudo(c)
+    c.sudo(f"systemctl restart {c.host}.service")
 
 
 def _get_sudo(c):
