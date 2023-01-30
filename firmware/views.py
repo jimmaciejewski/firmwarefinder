2de972e1f88f7597ad2e939ce47bfd342258e711
Jim Maciejewski <magicsoftware@ornear.com>
2023-01-30 10:02:30 +1000

    Testing local download button

--- firmware/views.py
+++ firmware/views.py
@@ -40,36 +40,11 @@ class ProductSearchView(ListView):
         if context['search'] is not None:
             # Since we search both current and discontinued product, don't highlight the NAV bar when results are shown...
             context['active'] = ""
-        return context
     
     def get_queryset(self):
-        """My attempt at getting the intersection of product fg and version fg 
-           I suspect this could be written more efficiently
+        """We populate the page with ajax
         """
-        products_with_versions = []
-        query = self.request.GET.get('q')
-        
-        if query is not None:
-            # If we are searching, search current and discontinued products
-            for product in Product.objects.all():
-                if Version.objects.filter(fgs__in=FG.objects.filter(product=product)):
-                    products_with_versions.append(product.id)
-            queryset = Product.objects.filter(id__in=products_with_versions).order_by('name')
-            # To search the read_me in the firmware versions -- first get the fgs with the search result
-            versions_fgs = Version.objects.filter(read_me__icontains=query).values_list('fgs')
-            versions_names_fgs = Version.objects.filter(name__icontains=query).values_list('fgs')
-        
-            # Then check if the fgs are in the product
-            return queryset.filter(Q(name__icontains=query) | 
-                                   Q(fgs__number__icontains=query) |
-                                   Q(fgs__in=versions_fgs) |
-                                   Q(fgs__in=versions_names_fgs))
-        else:
-            # Regular list here, just need to limit it to current or discontinued 
-            for product in Product.objects.filter(discontinued=self.discontinued):
-                if Version.objects.filter(fgs__in=FG.objects.filter(product=product)):
-                    products_with_versions.append(product.id)
-            return Product.objects.filter(discontinued=self.discontinued, id__in=products_with_versions).order_by('name')
+        return Product.objects.none()
 
 class ProductDetailView(DetailView):
     model = Product
@@ -113,48 +88,16 @@ def products_search(request):
         search_vector = SearchVector('read_me') + SearchVector('name')
         versions_fgs_list = Version.objects.annotate(search=search_vector).filter(search=search_query).values_list('fgs')
 
-        # versions_fgs = Version.objects.filter(read_me__icontains=query).values_list('fgs')
-        # versions_names_fgs = Version.objects.filter(name__icontains=query).values_list('fgs')
-        # # versions_fgs = Version.objects.annotate(search=SearchVector('read_me', 'name')).filter(search=query).values_list('fgs')
-
-        queryset = queryset.filter(Q(fgs__in=versions_fgs_list)).distinct()
+        queryset = queryset.filter(Q(fgs__in=versions_fgs_list))
             
-        # Q(name__icontains=query) | 
-        #                            Q(associated_names__name__icontains=query) |
-        #                            Q(fgs__number__icontains=query) |
-        
-    
-        # Then check if the fgs are in the product
-        vector = (SearchVector('name', weight='A') +
-                  SearchVector('associated_names', weight='B') +
-                  SearchVector('fgs__number', weight='C')
+        vector = (SearchVector('name') +
+                  SearchVector('associated_names') +
+                  SearchVector('fgs__number')
                   )
         search_query = SearchQuery(query)
         rate = SearchRank(vector, query)
         products = queryset.annotate(rate=rate).annotate(search=vector).distinct('name', 'rate').order_by('-rate', 'name')
 
-
-        # search_vector = SearchVector('name', weight='A') + SearchVector('associated_names__name', weight='B') + SearchVector('fgs__number')
-        
-        # products = queryset.annotate(
-        #     search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).distinct('rank', 'name').order_by('-rank', 'name')
-         
-
-
-
-
-        #    Q(fgs__in=versions_fgs)).distinct()
-
-        
-        # vector = SearchVector('name')
-        
-        # products = queryset.annotate(rank=SearchRank(vector, search_query)).order_by('-rank')
-
-        # products = queryset.filter(Q(name__icontains=query) | 
-        #                            Q(associated_names__name__icontains=query) |
-        #                            Q(fgs__number__icontains=query) |
-        #                            Q(fgs__in=versions_fgs) |
-        #                            Q(fgs__in=versions_names_fgs)).distinct()
     else:
         # Regular list here, just need to limit it to current or discontinued 
         for product in Product.objects.filter(discontinued=discontinued):
@@ -162,10 +105,28 @@ def products_search(request):
                 products_with_versions.append(product.id)
         products = Product.objects.filter(discontinued=discontinued, id__in=products_with_versions).order_by('name')
 
+
+    context= {"products": products}
+    client_ip = get_client_ip(request)
+    if client_ip == '127.0.0.1':
+        context['show_local'] = True
+    else:
+        print(f"From ip: {client_ip}")
+
+
     html = render_to_string(
         template_name="firmware/prod_list.html",
-        context={"products": products}
+        context=context
     )
 
     data_dict = {"html_from_view": html}
     return JsonResponse(data=data_dict, safe=False)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
