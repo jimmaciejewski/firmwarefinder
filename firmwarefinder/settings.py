54c752624ed6962c5e021e4a234e625c6e409ec6
Jim Maciejewski <magicsoftware@ornear.com>
2023-02-20 14:14:10 +1000

    testing azure storage

--- firmwarefinder/settings.py
+++ firmwarefinder/settings.py
@@ -133,11 +133,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / '../static/'
+DEFAULT_FILE_STORAGE = 'django_on_azure.backend.AzureMediaStorage'
+STATICFILES_STORAGE  = 'django_on_azure.backend.AzureStaticStorage'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / '../media/'
+AZURE_STORAGE_KEY = os.environ.get('AZURE_STORAGE_KEY', False)
+AZURE_ACCOUNT_NAME = "firmwaredemo"  # your account name
+AZURE_MEDIA_CONTAINER = os.environ.get('AZURE_MEDIA_CONTAINER', 'media')
+AZURE_STATIC_CONTAINER = os.environ.get('AZURE_STATIC_CONTAINER', 'static')
+
+# AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.azureedge.net'  # CDN URL
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'  # Files URL
+
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_STATIC_CONTAINER}/'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_MEDIA_CONTAINER}/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# any static paths you want to publish
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'demo', 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
