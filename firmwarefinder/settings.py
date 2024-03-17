3dd025edd6689278aaa89046ea385f390a7095e9
Jim Maciejewski <magicsoftware@ornear.com>
2024-03-18 09:56:48 +1000

    Testing remote_media via sshfs

--- firmwarefinder/settings.py
+++ firmwarefinder/settings.py
@@ -197,7 +197,7 @@ else:
     STATIC_ROOT = BASE_DIR / '../static/'
 
     MEDIA_URL = 'media/'
-    MEDIA_ROOT = BASE_DIR / '../media/'
+    MEDIA_ROOT = BASE_DIR / '../remote_media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
